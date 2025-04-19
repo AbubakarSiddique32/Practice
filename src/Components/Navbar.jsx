7b2a12167996ca7609605c9b7c00fedfcf133b5f
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-19 18:02:39 +0500

    Practice

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,16 +22,16 @@ const Navbar = () => {
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <a className="mr-5 hover:text-gray-900 cursor-pointer hover:font-bold">
-              First Link
+              Home
             </a>
             <a className="mr-5 hover:text-gray-900 cursor-pointer hover:font-bold">
-              Second Link
+              Data
             </a>
             <a className="mr-5 hover:text-gray-900 cursor-pointer hover:font-bold">
-              Third Link
+              About Us
             </a>
             <a className="mr-5 hover:text-gray-900 cursor-pointer hover:font-bold">
-              Fourth Link
+              Contact Us
             </a>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
