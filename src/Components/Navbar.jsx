18e3efd20ec08b43a074e11d9f41c4de9067bee9
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-19 18:06:10 +0500

    Practice

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             </a>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
+            Subscribe
             <svg
               fill="none"
               stroke="currentColor"
